510bcf5f49be76c49834404daf9add63b0a630ce
Arkadiusz Czechyra <41245831+AirGamming@users.noreply.github.com>
2023-01-08 22:36:02 +0100

    started main game loop

--- script.js
+++ script.js
@@ -11,21 +11,38 @@ let getComputerChoice = () =>{
 
 
 let playRound = (playerSelection, computerSelection) =>{
-    if (computerSelection == playerSelection){
+    if(playerSelection = ("rock"||"paper"||"scissors")){
+        return"Wrong choice computer won"
+    }else if (computerSelection == playerSelection){
         return"it's a tie!"
     }else if(
         (computerSelection == "rock" && playerSelection == "scissors")||
         (computerSelection == "paper" && playerSelection == "paper")||
         (computerSelection == "scissors" && playerSelection == "rock")
     ){
-        return"computer wins"
+        return "computer wins"
     }else{
-        return"You win"
+        return "You win"
     }
 }
-
-let game = () =>{
-    var yourPoints
-    var computerPoints
-    
-}
+//  FIXME:
+// let game = () =>{
+//     var playerPoints = 0
+//     var computerPoints = 0
+//     for(var i = 0; i >5; i++){
+//         var choice = "rock"
+//         var result = playRound(choice,getComputerChoice())
+//         if (result == ("Wrong choice computer won"||"computer wins")){
+//             computerPoints++
+//         }else if(result == "You win"){
+//             playerPoints++
+//         }else{
+//             i--
+//         }
+//     }
+//     if(playerPoints>computerPoints){
+//         alert("you won!")
+//     }else{
+//          alert("computer won")
+//     }
+// }
