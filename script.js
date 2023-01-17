19ebfb8d0629312efd8afe6e86a0f2e93623e0da
Arkadiusz Czechyra <41245831+AirGamming@users.noreply.github.com>
2023-01-17 18:18:01 +0100

    fixes to js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const regex = /(rock)|(paper)|(scissors)/g
-
 let getComputerChoice = () =>{
     let index = Math.floor(Math.random()*3)
     if(index == 1){
@@ -16,55 +14,41 @@ let playRound = (playerSelection, computerSelection = getComputerChoice()) =>{
     if(playerSelection != ("rock"||"paper"||"scissors")){
         return"Wrong choice computer won"
     }else if (computerSelection == playerSelection){
-        return 0
+        return [0,computerSelection]
     }else if(
         (computerSelection == "rock" && playerSelection == "scissors")||
         (computerSelection == "paper" && playerSelection == "paper")||
         (computerSelection == "scissors" && playerSelection == "rock")
     ){
-        return 1
+        return [1,computerSelection]
     }else{
-        return 2
+        return [2,computerSelection]
     }
 }
-const game =  () => {
-    var playerPoints = 0
-    var computerPoints = 0
-    var resoult
-    for(var i = 0; i < 5; i++){
-        var choice = prompt("rock, paper, scissors!")
-        choice = choice.toLocaleLowerCase()
-        while(choice == (!"rock"||!"paper"||!"scissors")){
-            choice = prompt("worng answer try again")
-            if (choice === null){
-                break
-            }else if(choice != "rock"||
-            choice != "paper"||
-            choice != "scissors"){
-                break
-            }
-        }
+//TODO:
+const rockIcon = '<i class="fa-solid fa-hand-back-fist rock"></i>'
+const paperIcon = '<i class="fa-solid fa-hand paper"></i>'
+const scissorsIcon = '<i class="fa-solid fa-hand-scissors scissors"></i>'
+const divResoult = document.querySelector('div.result')
+const whoWon = document.querySelector('.whoWon')
 
-        resoult = playRound(choice)
-        if(resoult == 1) {computerPoints = computerPoints + 1}
-        
-        else if (resoult == 2){playerPoints = playerPoints + 1}
-        
-        else{console.log("tie")}
-    }
-
-    if(computerPoints > playerPoints){return"computer won"}
-    
-    else if (computerPoints < playerPoints){return"you won"}
-    
-    else{return("tie " + computerPoints + " " + playerPoints)}
-}//TODO:
 const play = () => {
     console
     gameButtons.forEach(e => {
         if (!gameButtons) console.error("can't load game buttons")
         e.addEventListener('click', () =>{
-            playRound(e.dataset.choice)
+            a = playRound(e.dataset.choice)
+            divResoult.innerHTML = `<p>computer chose ${a[1]}, and you choose ${e.dataset.choice}</p>`
+            if(a[0] == 0){
+                whoWon.innerHTML = "It's a tie!"
+                whoWon.style.color = 'grey'
+            }else if(a[0] == 1){
+                whoWon.innerHTML = "Computer won!"
+                whoWon.style.color = 'red'
+            }else{
+                whoWon.innerHTML = "You won!"
+                whoWon.style.color = 'green'
+            }
     }) 
 
     });
